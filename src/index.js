e2ffd4e1b7ebe82bd8c95c8c8406e9bd2376d98e
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-04-27 22:08:18 +0200

    4. Objednávání

--- src/index.js
+++ src/index.js
@@ -12,3 +12,18 @@ navItems.forEach((navItem) => {
     navElm.classList.add('nav-closed');
   });
 });
+
+const orderBtn = document.querySelector('.order-btn');
+let ordered = false;
+
+orderBtn.addEventListener('click', () => {
+  const drinkCupElm = document.querySelector('.drink__cup');
+  ordered = !ordered;
+  if (ordered) {
+    drinkCupElm.classList.remove('drink__cup--selected');
+    orderBtn.textContent = 'Objednat';
+  } else {
+    drinkCupElm.classList.add('drink__cup--selected');
+    orderBtn.textContent = 'Zrušit';
+  }
+});
