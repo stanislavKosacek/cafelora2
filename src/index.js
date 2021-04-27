1c7902d317c304308278de0f3154ea7f56abafc4
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-04-27 22:02:17 +0200

    3. Zprovoznění navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
 
-console.log('funguju!');
+const navElm = document.querySelector('nav');
+document.querySelector('#nav-btn').addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+const navItems = navElm.querySelectorAll('a');
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', () => {
+    navElm.classList.add('nav-closed');
+  });
+});
